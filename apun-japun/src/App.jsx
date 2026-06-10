938c1230e32d0eeb67f62b3957d2012eaf807bdf
pupenderbeniwal2021-beep <pupenderbeniwal2021@gmail.com>
2026-06-10 08:50:41 +0000

    Updated React project

--- apun-japun/src/App.jsx
+++ apun-japun/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <h1>cool bro u dad is hear</h1>
+    <h1>i am hear </h1>
     </>
   )
 }
