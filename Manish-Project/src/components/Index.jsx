ac5324534fdaf7b47a9acb5e4350ce5050778c62
Choudhary_Manish_36 <choudharymanishgodara4@gmail.com>
2026-06-17 09:30:51 +0000

    Navbar added

---
+++ Manish-Project/src/components/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Css/Index.css';
+function Index (){
+    return(
+    <div className="MainCards">
+        <div className="Card">
+            <h1>Manish</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus rem eius aspernatur delectus tempora maiores.</p>
+            <button>View Profile</button>
+        </div>
+    </div>
+    )
+}
+export default Index
